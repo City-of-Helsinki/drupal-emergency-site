932eea19f115e39320cb486cb354788a17cc336f
Dragos Dumitrescu <dragos.dumitrescu@ibm.com>
2023-09-12 12:37:06 +0300

    refactoring iframe filter plugin

--- public/modules/custom/helfi_emergency_general/js/cookie_compliance_third_party.js
+++ public/modules/custom/helfi_emergency_general/js/cookie_compliance_third_party.js
@@ -4,29 +4,28 @@
       const prePreferencesLoadHandler = function (response) {
 
         // Add social feed iframes if consent for cookies was given.
-        if (response.currentStatus && context.cookie.includes('preference')) {
-          let iFrame = document.getElementsByTagName('iframe');
-          for (let i = 0; i < iFrame.length; i++) {
-            let srcValue = iFrame[i].getAttribute('data-cookie-compliance-src')
-            iFrame[i].setAttribute('src', srcValue)
-            let currentParent = iFrame[i].parentNode
-            let socialMediaWrapper = document.createElement('div')
-            socialMediaWrapper.className = 'social-media-wrapper'
-            currentParent.appendChild(socialMediaWrapper)
-            socialMediaWrapper.appendChild(iFrame[i])
+        let iFrames = document.querySelectorAll('iframe[data-cookie-compliance-src]');
+        let cookieAccepted = response.currentStatus && context.cookie.includes('preference');
+          iFrames.forEach(function (iframe) {
+          let currentParent = iframe.parentNode
+          let socialMediaWrapper = document.createElement('div')
+          socialMediaWrapper.className = 'social-media-iframe-wrapper'
+          currentParent.appendChild(socialMediaWrapper)
+          socialMediaWrapper.appendChild(iframe);
+
+          if (cookieAccepted) {
+            let srcValue = iframe.getAttribute('data-cookie-compliance-src')
+            iframe.setAttribute('src', srcValue)
+            iframe.removeAttribute('data-cookie-compliance-src');
           }
-        }
-        else {
-          let iFrame = document.getElementsByTagName('iframe');
-          for (let i = 0; i < iFrame.length; i++) {
-            let iframeDocument = iFrame[i].contentDocument || iFrame[i].contentWindow.document
-            console.log(context)
+          else {
+            let iframeDocument = iframe.contentDocument || iframe.contentWindow.document
             let divElement = iframeDocument.createElement("div")
             divElement.innerHTML = Drupal.t("Unable to load the content of this external frame because of your cookie settings." +
               " <br>Please accept or adjust your preferences for our cookie policy in order to view the content.");
             iframeDocument.body.appendChild(divElement);
           }
-        }
+        });
       };
 
       Drupal.eu_cookie_compliance('prePreferencesLoad', prePreferencesLoadHandler);
---
+++ public/modules/custom/helfi_emergency_general/src/Plugin/Filter/FilterIframeCookie.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\helfi_emergency_general\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Removes iframe src so that is not loaded if cookies have not been accepted.
+ *
+ * @Filter(
+ *   id = "filter_iframe_cookie",
+ *   title = @Translation("Filter Iframe Cookie"),
+ *   description = @Translation("Preventing iframe for being loaded if cookies have not been accepted."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ * )
+ */
+class FilterIframeCookie extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode): FilterProcessResult {
+    $html_dom = Html::load($text);
+    $iframes = $html_dom->getElementsByTagName('iframe');
+    foreach ($iframes as $iframe) {
+      $srcAttribute = $iframe->attributes->getNamedItem('src');
+      $iframe->removeAttribute('src');
+      $iframe->setAttribute('class', 'social-media-iframe');
+      $iframe->setAttribute('data-cookie-compliance-src', $srcAttribute->nodeValue);
+    }
+
+    $result = new FilterProcessResult(Html::serialize($html_dom));
+    $result->setAttachments([
+      'library' => ['helfi_emergency_general/helfi_emergency_third_party_cookie'],
+    ]);
+
+    return $result;
+  }
+
+}
