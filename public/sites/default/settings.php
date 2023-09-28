32c457ef66497c0a51215a04e185c6077684d070
ovidiu <ovidiu.ardelean@ibm.com>
2023-09-28 13:54:17 +0300

    added configs for smtp

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -280,7 +280,10 @@ if (getenv('SMTP_HOST')) {
   $config['smtp.settings']['smtp_password'] = getenv('MAILGUN_SMTP_PASSWORD');
   $config['smtp.settings']['smtp_from'] = getenv('MAILGUN_SMTP_USER');
   $config['smtp.settings']['smtp_fromname'] = getenv('MAILGUN_SMTP_USER');
-  $config['smtp.settings']['smtp_test_address'] = 'ovidiu.ardelean@ibm.com';
+  $config['smtp.settings']['smtp_on'] = true;
+  $config['smtp.settings']['smtp_protocol'] = 'tls';
+  $config['smtp.settings']['smtp_autotls'] = true;
+  $config['smtp.settings']['smtp_allowhtml'] = 1;
 }
 
 
