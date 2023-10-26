80aae16576a71f3c3f16149590f10d01dfdb9981
Dragos Dumitrescu <dragos.dumitrescu@ibm.com>
2023-10-26 12:04:02 +0300

    added sptm settings to all settings

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -349,16 +349,3 @@ if ($env = getenv('APP_ENV')) {
 if (empty($settings['deployment_identifier'])) {
   $settings['deployment_identifier'] = filemtime(__DIR__ . '/../../../composer.lock');
 }
-
-if (getenv('SMTP_HOST')) {
-  $config['smtp.settings']['smtp_host'] = getenv('SMTP_HOST');
-  $config['smtp.settings']['smtp_port'] = getenv('SMTP_PORT');
-  $config['smtp.settings']['smtp_username'] = getenv('MAILGUN_SMTP_USER');
-  $config['smtp.settings']['smtp_password'] = getenv('MAILGUN_SMTP_PASSWORD');
-  $config['smtp.settings']['smtp_from'] = getenv('MAILGUN_SMTP_USER');
-  $config['smtp.settings']['smtp_fromname'] = getenv('MAILGUN_SMTP_USER');
-  $config['smtp.settings']['smtp_on'] = true;
-  $config['smtp.settings']['smtp_protocol'] = 'tls';
-  $config['smtp.settings']['smtp_autotls'] = true;
-  $config['smtp.settings']['smtp_allowhtml'] = 1;
-}
